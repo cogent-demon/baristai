170ec165ab37ba1996b26e85f6ccbe70bc16eef9
cogent-demon <Cogentdemon@outlook.com>
2023-03-19 10:18:46 +0300

    rate-limit

--- routes/api/gpt.ts
+++ routes/api/gpt.ts
@@ -1,5 +1,6 @@
 import { HandlerContext } from "$fresh/server.ts";
 import "https://deno.land/x/dotenv/load.ts";
+import * as RateLimiterFlexible from "https://dev.jspm.io/rate-limiter-flexible";
 
 const SYSTEM_PROMPT = `
   You are a coffee expert and a barista that knows every kind of coffee.
@@ -11,29 +12,43 @@ const SYSTEM_PROMPT = `
 
 const OPEN_AI_API_KEY = Deno.env.get("OPENAI_API_KEY") ?? "";
 
-export const handler = async (req: Request, _ctx: HandlerContext) => {
-  const query = await req.text();
+const rateLimiter = new RateLimiterFlexible.default.RateLimiterMemory({
+  points: 1,
+  duration: 30,
+});
 
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${OPEN_AI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: SYSTEM_PROMPT },
-        { role: "user", content: query.substring(0, 280) },
-      ],
-      max_tokens: 200,
-    }),
-  });
+export const handler = async (req: Request, ctx: HandlerContext) => {
+  const { hostname } = ctx.remoteAddr as Deno.NetAddr;
+  console.log("hostname", hostname);
+  try {
+    await rateLimiter.consume('hostname', 1);
 
-  const json = await response.json();
-  console.log(json);
-  if (json.error) {
-    return new Response(json.error.message);
+    const query = await req.text();
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${OPEN_AI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: SYSTEM_PROMPT },
+          { role: "user", content: query.substring(0, 280) },
+        ],
+        max_tokens: 200,
+      }),
+    });
+
+    const json = await response.json();
+    console.log(json);
+    if (json.error) {
+      return new Response(json.error.message);
+    }
+    return new Response(json.choices?.[0].message.content);
+  } catch (e) {
+    console.log(e);
+    return new Response("BaristAI is busy with another customer right now. Please try again later.");
   }
-  return new Response(json.choices?.[0].message.content);
 };
