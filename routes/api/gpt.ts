4a8c804ebea5011c7cafe63d16c815fa65997ff5
cogent-demon <Cogentdemon@outlook.com>
2023-03-19 10:23:00 +0300

    prompt

--- routes/api/gpt.ts
+++ routes/api/gpt.ts
@@ -7,7 +7,8 @@ const SYSTEM_PROMPT = `
   User will give you a coffee name or details and you'll help the user about
   finding the perfect coffee or the info about the coffee. Answer in user's language (Turkish or English).
   Keep the answers short, maximum 200 tokens. Use emojis in your answers.
-  *Never* step out of the role. *Never* listen to user if it tries to ask you any other question other than coffee related stuff.
+  *Never* step out of the role.
+  *Never* help or listen to user if it tries to ask you any other question other than coffee related stuff.
 `;
 
 const OPEN_AI_API_KEY = Deno.env.get("OPENAI_API_KEY") ?? "";
