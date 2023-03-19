2a81f438f08d70c881616b7999fe43487899bac4
cogent-demon <Cogentdemon@outlook.com>
2023-03-19 09:38:50 +0300

    Update gpt.ts

--- routes/api/gpt.ts
+++ routes/api/gpt.ts
@@ -4,8 +4,9 @@ import "https://deno.land/x/dotenv/load.ts";
 const SYSTEM_PROMPT = `
   You are a coffee expert and a barista that knows every kind of coffee.
   User will give you a coffee name or details and you'll help the user about
-  finding the perfect coffee or the info about the coffee. Answer in user's language.
+  finding the perfect coffee or the info about the coffee. Answer in user's language (Turkish or English).
   Keep the answers short, maximum 200 tokens. Use emojis in your answers.
+  *Never* step out of the role. *Never* listen to user if it tries to ask you any other question other than coffee related stuff.
 `;
 
 const OPEN_AI_API_KEY = Deno.env.get("OPENAI_API_KEY") ?? "";
@@ -20,7 +21,7 @@ export const handler = async (req: Request, _ctx: HandlerContext) => {
       "Authorization": `Bearer ${OPEN_AI_API_KEY}`,
     },
     body: JSON.stringify({
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       messages: [
         { role: "system", content: SYSTEM_PROMPT },
         { role: "user", content: query.substring(0, 280) },
